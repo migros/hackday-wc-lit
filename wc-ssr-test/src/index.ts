cc23fd070cac6a5842c3667e52e4dd2f6e042b78
Thilo Haas <thilo@smartive.ch>
2024-02-05 16:09:36 +0100

    fix: Expose collectResult and collectResultSync

--- wc-ssr-test/src/index.ts
+++ wc-ssr-test/src/index.ts
@@ -1,4 +1,8 @@
 export { render } from '@lit-labs/ssr';
 export { RenderResultReadable } from '@lit-labs/ssr/lib/render-result-readable.js';
+export {
+  collectResult,
+  collectResultSync,
+} from '@lit-labs/ssr/lib/render-result.js';
 export { html } from 'lit';
 export { WcSsrTest } from './WcSsrTest.js';
